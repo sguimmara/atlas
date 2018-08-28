46930d8ec2860e6eaf2a9ba6f99b75fcf7d2ee86
sguimmara <sguimmara@pobox.com>
2018-08-28 23:24:42 +0200

    Mesh: implementation

--- src/atlas/graphics/Mesh.hpp
+++ src/atlas/graphics/Mesh.hpp
@@ -1,68 +1,39 @@
-//#ifndef ATLAS_MESH_HPP
-//#define ATLAS_MESH_HPP
-//
-//#include "AtlasGraphics.hpp"
-//
-//#include "Renderer2.hpp"
-//#include "Drawable.hpp"
-//#include "Shader.hpp"
-//
-//namespace atlas
-//{
-//    namespace graphics
-//    {
-//        class Mesh : public Drawable
-//        {
-//        public:
-//            Mesh(Renderer2* renderer, Shader* vertexShader, Shader* fragmentShader);
-//            ~Mesh();
-//
-//            VkCommandBuffer getCommandBuffer(uint32_t imageIndex);
-//            void prepareRenderState() override;
-//            void tick(float dt);
-//
-//            void setPositions(std::vector<glm::vec3>);
-//            void setColors(std::vector<glm::vec3>);
-//
-//        private:
-//            Renderer2 * renderer = nullptr;
-//            Shader* vertexShader = nullptr;
-//            Shader* fragmentShader = nullptr;
-//
-//            VkDevice device = VK_NULL_HANDLE;
-//            VkCommandPool pool = VK_NULL_HANDLE;
-//            VkPipelineLayout pipelineLayout = VK_NULL_HANDLE;
-//            VkPipeline pipeline = VK_NULL_HANDLE;
-//            std::vector<VkCommandBuffer> commandBuffers;
-//
-//            VkBuffer vertexBuffer = VK_NULL_HANDLE;
-//            VkDeviceMemory vertexBufferMemory = VK_NULL_HANDLE;
-//
-//            VkBuffer colorBuffer = VK_NULL_HANDLE;
-//            VkDeviceMemory colorBufferMemory = VK_NULL_HANDLE;
-//
-//            VkDescriptorPool descriptorPool = VK_NULL_HANDLE;
-//            VkDescriptorSetLayout descriptorSetLayout = VK_NULL_HANDLE;
-//            VkDescriptorSet descriptorSet = VK_NULL_HANDLE;
-//
-//            void createGraphicsPipeline();
-//            void destroyGraphicsPipeline();
-//            void createBuffer(VkBuffer&, void*, size_t, VkDeviceMemory&, VkBufferUsageFlags);
-//            void destroyBuffer(VkBuffer&, VkDeviceMemory&);
-//            void createCommandBuffer();
-//            void updateCommandBuffers();
-//            void destroyCommandBuffer();
-//            void createDescriptorSetLayout();
-//            void destroyDescriptorSetLayout();
-//            void createDescriptorPool();
-//            void destroyDescriptorPool();
-//            void createDescriptorSets();
-//            void destroyDescriptorSets();
-//            void updateDescriptorSets();
-//
-//            void setBuffer(void * data, size_t count, size_t elementSize, VkBuffer& target, VkDeviceMemory& mem, VkBufferUsageFlags flags);
-//        };
-//    }
-//}
-//
-//#endif
+#ifndef ATLAS_GRAPHICS_MESH_HPP
+#define ATLAS_GRAPHICS_MESH_HPP
+
+#include "AtlasGraphics.hpp"
+
+namespace atlas
+{
+    namespace graphics
+    {
+        struct Mesh
+        {
+            uint32_t vertexCount;
+            vk::Buffer indices;
+            vk::IndexType indexType;
+
+            vk::Buffer positions;
+            vk::Buffer normals;
+            vk::Buffer uv;
+
+            void Destroy(vk::Device device);
+
+        private:
+            vk::DeviceMemory indicesMemory;
+            vk::DeviceMemory positionsMemory;
+            vk::DeviceMemory normalsMemory;
+            vk::DeviceMemory uvMemory;
+
+            void SetIndices(vk::PhysicalDevice gpu, vk::Device device, std::vector<uint16_t> data);
+            void SetPositions(vk::PhysicalDevice gpu, vk::Device device, std::vector<glm::vec3> data);
+            void SetNormals(vk::PhysicalDevice gpu, vk::Device device, std::vector<glm::vec3> data);
+            void SetUV(vk::PhysicalDevice gpu, vk::Device device, std::vector<glm::vec2> data);
+            static void CreateBuffer(vk::PhysicalDevice gpu, vk::Device device,
+                void* data, size_t size, vk::BufferUsageFlags usage,
+                vk::Buffer& buf, vk::DeviceMemory& memory);
+        };
+    }
+}
+
+#endif
