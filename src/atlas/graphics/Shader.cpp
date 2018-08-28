5f43e686889544c22d9f7b98e902156f7bf95a1a
sguimmara <sguimmara@pobox.com>
2018-08-28 23:23:07 +0200

    Shader: formatting

--- src/atlas/graphics/Shader.cpp
+++ src/atlas/graphics/Shader.cpp
@@ -3,31 +3,33 @@
 
 namespace atlas
 {
-namespace graphics
-{
-    static vk::ShaderModule CreateShaderModule(const std::vector<char>& code, const vk::Device device)
+    namespace graphics
     {
-        auto const info = vk::ShaderModuleCreateInfo()
-            .setCodeSize(code.size())
-            .setPCode(reinterpret_cast<const uint32_t*>(code.data()));
+        static vk::ShaderModule CreateShaderModule(const std::vector<char>& code, const vk::Device device)
+        {
+            auto const info = vk::ShaderModuleCreateInfo()
+                .setCodeSize(code.size())
+                .setPCode(reinterpret_cast<const uint32_t*>(code.data()));
 
-        vk::ShaderModule module;
-        auto result = device.createShaderModule(&info, nullptr, &module);
-        VERIFY(result == vk::Result::eSuccess);
-        return module;
-    }
+            vk::ShaderModule module;
+            auto result = device.createShaderModule(&info, nullptr, &module);
+            VERIFY(result == vk::Result::eSuccess);
+            return module;
+        }
 
-    Shader::Shader(const std::string name, const std::string path, const vk::Device device) :
-        _name(name),
-        _device(device)
-    {
-        auto spirv = core::IO::readAllBytes(path);
-        _shaderModule = CreateShaderModule(spirv, _device);
-    }
+        Shader::Shader(const std::string name, const std::string path, const vk::Device device) :
+            _name(name),
+            _device(device)
+        {
+            auto spirv = core::IO::readAllBytes(path);
+            _shaderModule = CreateShaderModule(spirv, _device);
+            spdlog::get("renderer")->debug("loaded shader module '{0}'", path);
 
-    Shader::~Shader()
-    {
-        _device.destroyShaderModule(_shaderModule);
+        }
+
+        Shader::~Shader()
+        {
+            _device.destroyShaderModule(_shaderModule);
+        }
     }
-}
 }
